79a0f41e6f6b3b862e75cb8a91fa2d00d4aa3dac
samaoumou <i.ndiaye@isepdiamniadio.edu.sn>
2023-01-24 11:59:51 +0000

    cv

---
+++ app.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync( 'index.html');
+
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('/dev/ttyUSB0',{ 
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
+
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+var io = require('socket.io').listen(app);
+
+io.on('connection', function(socket) {
+    
+    console.log('Node is listening to port');
+    
+});
+
+parser.on('data', function(data) {
+  //ici on crée une variable temp qui pemet de séparer les valeurs de la temp et de l'hum  
+    console.log(data);
+    var temp = data.split(',')
+    io.emit('data', {'humidite': temp[0], 'temperature': temp[1]});
+    
+});
+
+app.listen(3000);
